5a30a3ffd25488d41e30eb6feade6f7f2e1856bb
m-zerbini <93603517+m-zerbini@users.noreply.github.com>
2021-12-12 14:07:31 +0100

    refactor 5

--- 5/5.js
+++ 5/5.js
@@ -1,23 +1,14 @@
 var fs = require('fs')
-var m = [];
 fs.readFile("./5/5.txt", "utf8", function (err, data) {
-    let l = data.split("\n");
-
+    let l = data.split("\n").map(el => el.replace(" -> ", ",").split(",").map(c => parseInt(c)));
+    var m = [];
+    
     for (let index = 0; index < l.length; index++) {
-        let [x1, y1, x2, y2] = l[index].replace(" -> ", ",").split(",").map(c => parseInt(c));
+        let [x1, y1, x2, y2] = l[index];
         if (x1 == x2 || y1 == y2) {
-            const isHorizontal = y1 == y2;
-            if (isHorizontal) {
-                [x1, x2] = x1 < x2 ? [x1, x2] : [x2, x1];
-                for (let x = x1; x <= x2; x++) {
-                    m[x + "-" + y1] = (m[x + "-" + y1] ?? 0)+1;
-                }
-            }
-            else {
-                [y1, y2] = y1 < y2 ? [y1, y2] : [y2, y1];
-                for (let y = y1; y <= y2; y++) {
-                    m[x1 + "-" + y] = (m[x1 + "-" + y] ?? 0)+1;
-                }
+            const [incx, incy] = [Math.sign(x2 - x1), Math.sign(y2 - y1)];
+            for (let x = x1, y = y1; x != x2 + incx || y != y2 + incy; x += incx, y += incy) {
+                m[`${x}-${y}`] = (m[`${x}-${y}`] ?? 0) + 1;
             }
         }
     }
