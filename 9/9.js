5ae7a1697021ba038662d9f46ef4bbbfd73a699c
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-12 21:08:30 +0100

    cleanup

--- 9/9.js
+++ 9/9.js
@@ -1,21 +1,14 @@
-
 var fs = require('fs')
 
 fs.readFile("./9/input.txt", "utf8", function (err, data) {
-    let l = data.split("\n").map((e)=>e.split('').map(n=>parseInt(n)));
-    
+    let l = data.split("\n").map((e) => e.split('').map(n => parseInt(n)));
     let sum = 0;
     for (let r = 0; r < l.length; r++) {
-        const row = l[r];
-        for (let c = 0; c < row.length; c++) {
-            const col = row[c];
-            let adjacent = [(l[r-1]??[])[c]??9, (l[r+1]??[])[c]??9, (l[r]??[])[c-1]??9, (l[r]??[])[c+1]??9]
-            var isLower =  adjacent.reduce((p,c)=> p && c>col, true);
-            if(isLower){
-                sum += col+1
-            }
+        for (let c = 0; c < l[r].length; c++) {
+            let adjacent = [(l[r - 1] ?? [])[c] ?? 9, (l[r + 1] ?? [])[c] ?? 9, (l[r] ?? [])[c - 1] ?? 9, (l[r] ?? [])[c + 1] ?? 9]
+            var isLower = adjacent.reduce((p, c) => p && c > l[r][c], true);
+            sum += isLower ? l[r][c] + 1 : 0;
         }
-        
     }
     console.log(sum);
 });
