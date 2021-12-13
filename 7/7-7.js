6ad5e9ac35118e4c9c8f2aed743088b86e7973bc
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-14 00:00:14 +0100

    day 7 part 2

--- 7/7-7.js
+++ 7/7-7.js
@@ -1,8 +1,17 @@
-var fs = require('fs')
-var m = [];
-fs.readFile("./7/input.txt", "utf8", function (err, data) {
-    let l = data.split(",").map(s => parseInt(s)).sort((a, b) => a - b);
-//??
+const fs = require('fs');
+//var f = (i)=> i == 1 ? 1 : f(i - 1) + i;
+const f = (n) => (n * (n + 1) / 2);
 
-    console.log(energy);
-});
+let l = fs.readFileSync("./7/input.txt", "utf8").split(",").map(s => parseInt(s)).sort((a, b) => a - b);
+
+let map = l.reduce((p, c) => {
+    p[c] = (p[c] ?? 0) + 1;
+    return p;
+}, {});
+
+let currentCost = Number.MAX_VALUE;
+for (let pos = 0; pos <= l[l.length - 1]; pos++) {
+    let cost = [...Object.entries(map)].reduce((c, [position, count]) => c + f(Math.abs(pos - parseInt(position))) * count, 0);
+    if (cost < currentCost) currentCost = cost;
+}
+console.log(currentCost);
