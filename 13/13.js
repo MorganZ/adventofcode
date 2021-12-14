3f02cca68efb85163927cf1599518ed2043480ae
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-14 20:55:59 +0100

    d13 rewrite

--- 13/13.js
+++ 13/13.js
@@ -1,24 +1,13 @@
-const fs = require('fs');
-const input = fs.readFileSync("./13/input.txt", "utf8");
-const lines = input.split("\r\n")
+const lines = require('fs').readFileSync("./13/input.txt", "utf8").split("\n\n").map(l=>l.split("\n"));
+let points = lines[0].map(l => l.split(",").map(c => +c));
+let folds = lines[1].map(l => l.replace("fold along ", "").split('=')).map(([xy, f]) => [xy == 'x' ? 0 : 1, +f]);
 
-let points = lines.filter(l => !l.startsWith("f") && l)
-    .map(l => l.split(",").map(coord => parseInt(coord)));
-let folds = lines.filter(l => l.startsWith("f") && l)
-    .map(l => l.replace("fold along ", "").split('=')).map(([axis, foldPos]) => [axis, parseInt(foldPos)]);
-
-for ([axis, foldPos] of folds) {
-    for (let p = 0; p < points.length; p++) {
-        ax = axis == "x" ? 0 : 1;
-        if (points[p][ax] > foldPos)
-            points[p][ax] = foldPos - (points[p][ax] - foldPos)
+for ([ax, seam] of folds) 
+    for (let p of points) {
+        if (p[ax] > seam)
+            p[ax] = seam - (p[ax] - seam)
     }
-}
 
-var t = [... new Set(points.map(p => p.join(',')))].map(p => p.split(',').map(c => parseInt(c)));
-
-var array = new Array(100);
-for (let i = 0; i < 100; i++)  array[i] = new Array(100).fill(' ');
-for (let i = 0; i < t.length; i++)  array[t[i][1]][t[i][0]] = "x";
-
-console.log(array);
+var display = Array(50).fill(null).map((e) => Array(50).fill(' '));
+points.forEach((pt)=> display[pt[1]][pt[0]] = "x");
+display;
