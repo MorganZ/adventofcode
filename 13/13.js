aab375456b579aef3ca25f0f0fde539d610221f0
m-zerbini <93603517+m-zerbini@users.noreply.github.com>
2021-12-14 15:00:29 +0100

    day 13 clean

--- 13/13.js
+++ 13/13.js
@@ -10,15 +10,15 @@ let folds = lines.filter(l => l.startsWith("f") && l)
 for ([axis, foldPos] of folds) {
     for (let p = 0; p < points.length; p++) {
         ax = axis == "x" ? 0 : 1;
-        if (points[p][ax] > foldPos) 
+        if (points[p][ax] > foldPos)
             points[p][ax] = foldPos - (points[p][ax] - foldPos)
-
     }
 }
+
 var t = [... new Set(points.map(p => p.join(',')))].map(p => p.split(',').map(c => parseInt(c)));
 
 var array = new Array(100);
-for (let i = 0; i < 100; i++)  array[i]=new Array(100).fill(' ');
+for (let i = 0; i < 100; i++)  array[i] = new Array(100).fill(' ');
 for (let i = 0; i < t.length; i++)  array[t[i][1]][t[i][0]] = "x";
 
 console.log(array);
