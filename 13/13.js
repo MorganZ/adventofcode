114cb5d9bc52f80fbdeac2a4ab7f6650f9d27c77
m-zerbini <93603517+m-zerbini@users.noreply.github.com>
2021-12-14 14:58:34 +0100

    Day 13 part 2

--- 13/13.js
+++ 13/13.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 const input = fs.readFileSync("./13/input.txt", "utf8");
 const lines = input.split("\r\n")
 
@@ -7,25 +6,19 @@ let points = lines.filter(l => !l.startsWith("f") && l)
     .map(l => l.split(",").map(coord => parseInt(coord)));
 let folds = lines.filter(l => l.startsWith("f") && l)
     .map(l => l.replace("fold along ", "").split('=')).map(([axis, foldPos]) => [axis, parseInt(foldPos)]);
+
 for ([axis, foldPos] of folds) {
-    switch (axis) {
-        case 'x':
-            for (let p = 0; p < points.length; p++) {
-                const point = points[p][0];
-                if(point > foldPos)
-                    points[p][0] = foldPos-(points[p][0]-foldPos)
-            }
-            break;
-        case 'y':
-            for (let p = 0; p < points.length; p++) {
-                const point = points[p][1];
-                if(point > foldPos)
-                    points[p][1] = foldPos-(points[p][1]-foldPos)
-            }
-            break;
+    for (let p = 0; p < points.length; p++) {
+        ax = axis == "x" ? 0 : 1;
+        if (points[p][ax] > foldPos) 
+            points[p][ax] = foldPos - (points[p][ax] - foldPos)
+
     }
 }
+var t = [... new Set(points.map(p => p.join(',')))].map(p => p.split(',').map(c => parseInt(c)));
 
-var t = new Set(points.map(p=>p.join('-')));
+var array = new Array(100);
+for (let i = 0; i < 100; i++)  array[i]=new Array(100).fill(' ');
+for (let i = 0; i < t.length; i++)  array[t[i][1]][t[i][0]] = "x";
 
-console.log(points);
+console.log(array);
