e86773b77c7e44115841035d07565b6dd4a86959
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-16 15:22:24 +0100

    day 4 rework

--- 4/4-4.js
+++ 4/4-4.js
@@ -1,41 +1,24 @@
-var fs = require('fs')
+const lines = require('fs').readFileSync("./4/4.txt", "utf8").split("\n\n");
+const bingoNumbers = lines.shift().split(",").map(n => +n)
+const markedBoard = lines.map(() => Array(25).fill(0));
+const boards = lines.map(l => l.replace(/\n/g, ' ').split(' ').filter((e) => e != '').map(n => +n));
+const boardsWin = new Set();
 
-fs.readFile("./4/4.txt", "utf8", function (err, data) {
-    const l = data.split("\n\n");
-    const bingo = l.shift().split(",");
-    const markedBoard = []
-    const boards = []
-    const boardsWin = new Set();
-    for (let index = 0; index < l.length; index++) {
-        const board = l[index];
-        markedBoard[index] = Array(25).fill(0);
-        boards[index] = board.replace(/\n/g, ' ').split(' ').filter((e) => e != '');
-    }
-
-    for (let i = 0; i < bingo.length; i++) {
-        const bingoNumber = bingo[i];
-        for (let bi = 0; bi < boards.length; bi++) {
-            if(boardsWin.has(bi)) continue;
-            const b = boards[bi];
-            const mb = markedBoard[bi]
-            let ii = b.findIndex((v) => v == bingoNumber);
-            if (ii == -1) continue;
-            mb[ii] = 1;
-            const ri = (ii - ii % 5);
-            const ci = ii % 5;
-            if (mb[ri] + mb[ri + 1] + mb[ri + 2] + mb[ri + 3] + mb[ri + 4] == 5 ||
-                mb[ci] + mb[ci + 5 * 1] + mb[ci + 5 * 2] + mb[ci + 5 * 3] + mb[ci + 5 * 4] == 5) {
-                boardsWin.add(bi);
-                let total = 0;
-                for (let mi = 0; mi < mb.length; mi++) {
-                    if (mb[mi] == 0) {
-                        total += parseInt(b[mi]);
-                    }
-                }
-                if(boardsWin.size == boards.length){
-                    console.log(total * bingoNumber);
-                }
+for (let bingoNumber of bingoNumbers)
+    for (let bi = 0; bi < boards.length; bi++) {
+        if (boardsWin.has(bi)) continue;
+        const b = boards[bi], mb = markedBoard[bi];
+        let foundIndex = b.findIndex((v) => v == bingoNumber);
+        if (foundIndex == -1) continue;
+        mb[foundIndex] = 1;
+        const ri = (foundIndex - foundIndex % 5);//row index
+        const ci = foundIndex % 5; //col index
+        if (mb[ri] + mb[ri + 1] + mb[ri + 2] + mb[ri + 3] + mb[ri + 4] == 5 ||
+            mb[ci] + mb[ci + 5 * 1] + mb[ci + 5 * 2] + mb[ci + 5 * 3] + mb[ci + 5 * 4] == 5) {
+            boardsWin.add(bi);
+            let score = mb.reduce((p, m, i) => m ? p : p + parseInt(b[i]), 0);
+            if (boardsWin.size == boards.length) {
+                console.log(score * bingoNumber); return;
             }
         }
     }
-});
--- 4/4.js
+++ 4/4.js
@@ -1,37 +1,20 @@
-var fs = require('fs')
 
-fs.readFile("./4/4.txt", "utf8", function (err, data) {
-    const l = data.split("\n\n");
-    const bingo = l.shift().split(",");
-    const markedBoard = []
-    const boards = []
-    for (let index = 0; index < l.length; index++) {
-        const board = l[index];
-        markedBoard[index] = Array(25).fill(0);
-        boards[index] = board.replace(/\n/g, ' ').split(' ').filter((e) => e != '');
-    }
+const lines = require('fs').readFileSync("./4/4.txt", "utf8").split("\n\n");
+const bingoNumbers = lines.shift().split(",").map(n => +n)
+const markedBoard = lines.map(() => Array(25).fill(0));
+const boards = lines.map(l => l.replace(/\n/g, ' ').split(' ').filter((e) => e != '').map(n => +n));
 
-    for (let i = 0; i < bingo.length; i++) {
-        const bingoNumber = bingo[i];
-        for (let bi = 0; bi < boards.length; bi++) {
-            const b = boards[bi];
-            const mb = markedBoard[bi]
-            let ii = b.findIndex((v) => v == bingoNumber);
-            if (ii == -1) continue;
-            mb[ii] = 1;
-            const ri = (ii - ii % 5);
-            const ci = ii % 5;
-            if (mb[ri] + mb[ri + 1] + mb[ri + 2] + mb[ri + 3] + mb[ri + 4] == 5 ||
-                mb[ci] + mb[ci + 5 * 1] + mb[ci + 5 * 2] + mb[ci + 5 * 3] + mb[ci + 5 * 4] == 5) {
-                let total = 0;
-                for (let mi = 0; mi < mb.length; mi++) {
-                    if (mb[mi] == 0) {
-                        total += parseInt(b[mi]);
-                    }
-                }
-                console.log(total * bingoNumber);
-                return;
-            }
+for (let bingoNumber of bingoNumbers)
+    for (let bi = 0; bi < boards.length; bi++) {
+        const b = boards[bi], mb = markedBoard[bi];
+        let foundIndex = b.findIndex((v) => v == bingoNumber);
+        if (foundIndex == -1) continue;
+        mb[foundIndex] = 1;
+        const ri = (foundIndex - foundIndex % 5);//row index
+        const ci = foundIndex % 5; //col index
+        if (mb[ri] + mb[ri + 1] + mb[ri + 2] + mb[ri + 3] + mb[ri + 4] == 5 ||
+            mb[ci] + mb[ci + 5 * 1] + mb[ci + 5 * 2] + mb[ci + 5 * 3] + mb[ci + 5 * 4] == 5) {
+            let score = mb.reduce((p, m, i) => m ? p : p + parseInt(b[i]), 0);
+            console.log(score * bingoNumber);
         }
     }
-});
