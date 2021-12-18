f201d3f47db5d83dbe2196d64af2d907f326457f
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-18 07:52:22 +0100

    Update 6-6.js
    Day 6 rework with array

--- 6/6-6.js
+++ 6/6-6.js
@@ -1,19 +1,8 @@
 var input = require('fs').readFileSync("./6/6.txt", "utf8").split(",").map(e => parseInt(e));
-let baseMap = new Map(Array(9).fill(0).map((_, index) => [index, 0]))
-var fishes = new Map(baseMap.entries());
-
-for (let age of input){
-    fishes.set(age, fishes.get(age) + 1)
-}
+var fishes = input.reduce((p, c) => (p[c] += 1, p), Array(9).fill(0));
 
 for (let day = 0; day < 256; day++) {
-    const tfs = new Map(baseMap.entries());
-    let newFish = fishes.get(0);
-    for (let age = 8; age >= 1; age--) {
-        tfs.set(age - 1, fishes.get(age));
-    }
-    tfs.set(6, tfs.get(6) + newFish);
-    tfs.set(8, newFish);
-    fishes = tfs;
-} 
-console.log([...fishes.values()].reduce((p, c) => p + c, 0));
+    fishes.push(fishes.shift());
+    fishes[6] += fishes[8];
+}
+console.log([...Object.values(fishes)].reduce((p, c) => p + c, 0));
