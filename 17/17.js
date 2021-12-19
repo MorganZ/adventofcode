1718208050dd4f1b6df93093d1548b682bc66578
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-19 10:41:00 +0100

    D17 update part 1

--- 17/17.js
+++ 17/17.js
@@ -1,6 +1,12 @@
 let data = require('fs').readFileSync("./17/input.txt", "utf8");
 [_, ylimit] = data.slice(13, data.length).split(', ').map(c => c.split("=")[1].split('..').map(c => +c));
 
-let lastStepYVelocity = Math.abs(ylimit[0]); 
-let ymax = lastStepYVelocity*(lastStepYVelocity-1)/2;
-console.log(ymax);
+if(ylimit[0]<=0 && 0 <=ylimit[1]){
+    console.log('infinit alt');
+}else{
+    let [ymin, ymax] = [Math.abs(ylimit[0]),Math.abs(ylimit[1])];
+    let lastVelBeforeZero = ymin > ymax ? ymin - 1 : ymax + 1;
+    let alt = lastVelBeforeZero * (lastVelBeforeZero + 1) / 2;
+    console.log(alt);
+}
+
