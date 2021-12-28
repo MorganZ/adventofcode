8dec9aafe8a016e498270a6b3d95aa7395d6a651
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-28 01:10:50 +0100

    Day 21 part 2

--- 21/21-21.js
+++ 21/21-21.js
@@ -1,26 +1,35 @@
 //Not working solution for now 
+//157253621231420
 
 let pid = 1;
-let start = [new Map([['1,0', 0]]), new Map([['6,0', 0]])];
-let diceValues = [1, 2, 3];
-let possibleRolls = Object.entries(diceValues.flatMap(d1 => diceValues.flatMap(d2 => diceValues.flatMap(d3 => d1 + d2 + d3))).reduce((p, c) => (p[c] ? p[c]++ : p[c] = 1, p), {}));
+let start = [new Map([['1,0', 1]]), new Map([['6,0', 1]])];
+//let possibleRolls = Object.entries({ 3: 1n, 4: 3n, 5: 6n, 6: 7n, 7: 6n, 8: 3n, 9: 1n })
+//let possibleRolls = [["3",1n],["4",3n],["5",6n],["6",7n],["7",6n],["8",3n],["9",1n]]
+let possibleRolls = [[3, 1], [4, 3], [5, 6], [6, 7], [7, 6], [8, 3], [9, 1]]
 let pc = [0, 0];
-while (start[0].size > 0 || start[1].size > 0) {
-    console.log(start);
+let total_prec = 0;
+while (start[0].size > 0 && start[1].size > 0) {
     pid ^= 1;
+    let total_universe = 0;
+    let temp_score = start[pid].entries();
     let newScores = new Map();
-    for (let [key, count] of start[pid].entries()) {
-        for (let [d, c] of possibleRolls) {
-            let [p, ps] = key.split(',').map(c => +c);
-            p = (p + d - 1) % 10 + 1;
-            ps += p;
-            newScores.set(p + "," + ps, (newScores.get(p + "," + ps) ?? 1) + c * count);
-            if (ps >= 21) {
-                pc[pid] = pc[pid] + newScores.get(p + "," + ps);
-                newScores.delete(p + "," + ps);
+    for (let [key, uCount] of temp_score) {
+        for (let [d, duCount] of possibleRolls) {
+            let [pos, score] = key.split(',').map(c => +c);
+            pos = (pos + d - 1) % 10 + 1;
+            score += pos;
+            let newUCount = uCount * duCount;
+            if (score < 21) {
+                let key = `${pos},${score}`;
+                total_universe += newUCount;
+                newScores.set(key, (newScores.get(key) ?? 0) + newUCount);
+            }
+            else {
+                pc[pid] += newUCount * total_prec
             }
         }
     }
+    total_prec = total_universe
     start[pid] = newScores;
 }
 console.log(pc);
