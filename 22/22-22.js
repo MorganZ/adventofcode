a3751e74ac67e9e60b0c5b92a91712a6db7569cf
MorganZ <4096281+MorganZ@users.noreply.github.com>
2021-12-28 13:32:49 +0100

    Day 22 part 1 and work on part 2

--- 22/22-22.js
+++ 22/22-22.js
@@ -1,26 +1,42 @@
-let data = require('fs').readFileSync('./22/input.txt', 'utf-8').split('\n');
+//not working solution
+let lines = require('fs').readFileSync('./22/input.txt', 'utf-8').split('\n');
+let boundingBoxes = lines.map(line => ({ on: line.startsWith('on'), bBox: line.split(',').map(p => [...p.matchAll(/(-?\d+)/g)].map(e => +e[0])) }));
+
 let isIntersecting = (a, b) => a.map(([min, max], i) => min > b[i][1] || max < b[i][0]).some(a => !a);
 let intersect = (a, b) => a.map(([min, max], i) => [Math.max(min, b[i][0]), Math.min(max, b[i][1])])
+let volume = (c) => c.reduce((p, c) => (p = p * (c[1] - c[0] + 1), p), 1)
 
-let boxes = data.map(d => {
-    let on = d.startsWith('on');
-    let bBox = d.split(',').map(l => [...l.matchAll(/(-?\d+)/g)].map(e => +e[0]));
-    return { on, bBox }
-});
+console.log(isIntersecting([[0, 10]], [[11, 12]]));
+console.log(isIntersecting([[0, 10]], [[10, 11]]));
+console.log(isIntersecting([[0, 10]], [[5, 10]]));
+
+console.log(intersect([[0, 10]], [[11, 12]]));
+console.log(intersect([[0, 10]], [[10, 11]]));
+console.log(intersect([[0, 10]], [[5, 10]]));
+console.log(intersect([[0, 10], [0, 10]], [[5, 10],[5, 10]]));
+
+console.log(volume([[0, 9], [0, 9]]) == 100);
+console.log(volume([[0, 9], [0, 9], [0, 9]]) == 1000);
 
 
-let newBoxes = [];
-
-boxes.forEach(b => {
-    let _boxes = [];
-    if (b.on) _boxes.push(b);
-    for (let bb of newBoxes) {
-        if (isIntersecting(b.bBox, bb.bBox))
-            _boxes.push({ on: !bb.on, bBox: intersect(b.bBox, bb.bBox) });
+let bBoxes = [];
+boundingBoxes.forEach(b => {
+    const newBoxes = [];
+    if (b.on) newBoxes.push(b);
+    for (let bb of bBoxes) {
+        if (isIntersecting(b.bBox, bb.bBox)) {
+            newBoxes.push({ on: !bb.on, bBox: intersect(b.bBox, bb.bBox) });
+        }
     }
 
-    newBoxes = newBoxes.concat(_boxes);
-    console.log(newBoxes.length)
+    bBoxes = bBoxes.concat(newBoxes);
+
+    console.log(bBoxes.length)
 })
 
-console.log(newBoxes);
+let rrrr = bBoxes.reduce((p, b) => p + volume(b.bBox) * (b.on ? 1 : -1), 0)
+
+console.log(rrrr);
+
+
+
