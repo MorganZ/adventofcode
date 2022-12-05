c03187f254297b93333a92a5809de8d60066df3a
ZM <zerbini.morgan@gmail.com>
2022-12-05 21:57:58 +0100

    using splice and parsing earlier

--- Advent2022/5/5_2.js
+++ Advent2022/5/5_2.js
@@ -1,12 +1,9 @@
-const [sstack, smove] = require('fs').readFileSync("./5/input.txt", 'utf8').split('\r\n\r\n').map((c) => c.split('\r\n'));
-const stacks = new Array(9).fill(0).map(c => []);
-sstack.forEach(c => stacks.forEach((s, i) => c[1 + (i * 4)] != ' ' ? s.unshift(c[1 + (i * 4)]) : null));
-const moves = smove.map(c => c.split(' ').filter((v, i) => (i + 1) % 2 == 0).map(c => parseInt(c)))
+const [lstack, lmove] = require('fs').readFileSync("./5/input.txt", 'utf8').split('\r\n\r\n').map((c) => c.split('\r\n'));
+const stacks = Array.from({length:9},()=> [])
+lstack.forEach(c => stacks.forEach((s, i) => c[1 + (i * 4)] != ' ' ? s.unshift(c[1 + (i * 4)]) : null));
+const moves = lmove.map(c => c.split(' ').filter((v, i) => (i + 1) % 2 == 0).map((c, i ) =>  parseInt(c) + (i>0?-1:0)))
 
-const tempStack = [];
-for (const [count, from, to] of moves) {
-    for (let i = 0; i < count; i++) tempStack.push(stacks[from - 1].pop());
-    for (let i = 0; i < count; i++) stacks[to - 1].push(tempStack.pop());
-}
+for (const [count, from, to] of moves) 
+    stacks[to].push(...stacks[from].splice(stacks[from].length-count, count));
 
 console.log(stacks.map(c => c.pop()).join(''));
