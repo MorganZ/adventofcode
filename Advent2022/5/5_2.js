8b2f7197e7d25d10d0f0d7ccc9aad3de3c2f456a
ZM <zerbini.morgan@gmail.com>
2022-12-05 18:02:37 +0100

    rework parsing and simplify

--- Advent2022/5/5_2.js
+++ Advent2022/5/5_2.js
@@ -1,32 +1,12 @@
-
-const inputs = require('fs').readFileSync("./5/input.txt", 'utf8').split('\r\n\r\n').map((c)=> c.split('\r\n'));
-
-[sstack, smove] = inputs;
-
-const stacks = new Array(9).fill(0).map(c=>[]);
-
-for (let l = 0; l < sstack.length-1; l++) {
-    const element = sstack[l];    
-    for (let index = 0; index < 9; index++) {
-        const blockId = element[1+(index*4)];
-        if(blockId != ' '){
-            stacks[index].unshift(blockId);
-        }
-    }
-}
-
-const moves = smove.map(c=> c.split(' ').filter((v, i)=> (i+1)%2 == 0).map(c=>parseInt(c)))
+const [sstack, smove] = require('fs').readFileSync("./5/input.txt", 'utf8').split('\r\n\r\n').map((c) => c.split('\r\n'));
+const stacks = new Array(9).fill(0).map(c => []);
+sstack.forEach(c => stacks.forEach((s, i) => c[1 + (i * 4)] != ' ' ? s.unshift(c[1 + (i * 4)]) : null));
+const moves = smove.map(c => c.split(' ').filter((v, i) => (i + 1) % 2 == 0).map(c => parseInt(c)))
 
 const tempStack = [];
-for(const [count, from, to] of moves){
-    for (let i = 0; i < count; i++) {        
-        tempStack.push(stacks[from-1].pop());
-    }
-
-    for (let i = 0; i < count; i++) {        
-        stacks[to-1].push(tempStack.pop());
-    }
-
+for (const [count, from, to] of moves) {
+    for (let i = 0; i < count; i++) tempStack.push(stacks[from - 1].pop());
+    for (let i = 0; i < count; i++) stacks[to - 1].push(tempStack.pop());
 }
 
-console.log(stacks.map(c=>c.pop()).join(''));
+console.log(stacks.map(c => c.pop()).join(''));
