547c6916502de4800ca2c9bc4479447b74aae39a
ZM <zerbini.morgan@gmail.com>
2022-12-07 09:25:21 +0100

    day 7 50m

---
+++ Advent2022/7/7.js
@@ -0,0 +1,45 @@
+const input = require('fs').readFileSync('./7/input.txt', 'utf-8').split("\r\n")
+
+const root = { dir: "@", children: [] };
+const AnalyserConsoleOutput = (currentDir, input) => {
+  if (input.length === 0 && currentDir.dir == "@") return;
+  if (input.length === 0)  input.push("$ cd ..");
+  
+  const data = input.shift().split(" ");
+  switch (data[0] === "$" ? data[1] : data[0]) {
+    case "cd":
+      const dirName = data[2];
+      switch (dirName) {
+        case "/":
+          currentDir = { dir: dirName, children: [], parent: currentDir };
+          root.children.push(currentDir);
+          break;
+        case "..":
+          currentDir.size = currentDir.children.reduce((p, child) => p + child.size, 0);
+          currentDir = currentDir.parent;
+          break;
+        default:
+          currentDir = currentDir.children.find(child => child.dir === dirName);
+      }
+      break;
+    case "ls": break;
+    case "dir":
+      currentDir.children.push({ dir: data[1], children: [], parent: currentDir });
+      break;
+    default:
+      currentDir.children.push({ file: data[1], size: parseInt(data[0]), parent: currentDir });
+  }
+  AnalyserConsoleOutput(currentDir, input)
+}
+
+AnalyserConsoleOutput(root, input);
+
+// 1
+const sumDirLowerThan100000 = (dir) => dir.children.reduce((p, child) => p + (child.dir ? sumDirLowerThan100000(child) : 0), dir.size <= 100000 ? dir.size : 0);
+console.log(sumDirLowerThan100000(root.children[0]));
+
+// 2
+let space = Math.abs(70000000 - 30000000 - root.children[0].size)
+const findAll = (dir) => dir.children.reduce((p, child) => (child.dir ? p.push(...findAll(child)) : p, p), dir.size > space ? [dir] : []);
+
+console.log(findAll(root.children[0]).sort((a, b) => a.size - b.size)[0].size);
