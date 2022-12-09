68d67112c8015d055aaa0234fcdd8ed91833b288
zm <morgan@z-rational.com>
2022-12-09 20:02:52 +0100

    if no change stop earlier with break

--- Advent2022/9/9.js
+++ Advent2022/9/9.js
@@ -1,5 +1,5 @@
 const dirToVector = { U: { x: 0, y: 1 }, D: { x: 0, y: -1 }, L: { x: -1, y: 0 }, R: { x: 1, y: 0 } }
-const moves = require('fs').readFileSync('./9/input.txt', 'utf-8').split('\n').map(move => [dirToVector[move[0]] , parseInt(move.slice(1))]);
+const moves = require('fs').readFileSync('./9/input.txt', 'utf-8').split('\n').map(move => [dirToVector[move[0]], parseInt(move.slice(1))]);
 
 [2, 10].map(ropeSize => {
     const rope = new Array(ropeSize).fill(0).map(c => ({ x: 0, y: 0 }));
@@ -10,9 +10,11 @@ const moves = require('fs').readFileSync('./9/input.txt', 'utf-8').split('\n').m
             head.x += direction.x, head.y += direction.y;
             for (let index = 1; index < rope.length; index++) {
                 const head = rope[index - 1], tail = rope[index];
-                if (Math.abs(head.x - tail.x) > 1 || Math.abs(head.y - tail.y) > 1)
+                if (Math.abs(head.x - tail.x) > 1 || Math.abs(head.y - tail.y) > 1) {
                     tail.x += Math.sign(head.x - tail.x), tail.y += Math.sign(head.y - tail.y);
-                if (index === rope.length - 1) visited.add(`${tail.x},${tail.y}`);
+                    if (index === rope.length - 1) visited.add(`${tail.x},${tail.y}`);
+                }
+                else break;
             }
         }
     }
