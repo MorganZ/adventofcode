5d228e28962b545b5f62867c0cd109d546e22e8d
zm <morgan@z-rational.com>
2022-12-12 18:09:51 +0100

    update day 12

--- Advent2022/12/12_2.js
+++ Advent2022/12/12_2.js
@@ -1,31 +1,20 @@
 const input = require('fs').readFileSync('./12/input.txt', 'utf-8');
-
 const map = input.replace('S', 'a').replace('E', 'z').split("\n").map(l => l.split('').map(e => +e.charCodeAt(0) - 97))
-const start = (pos = input.replaceAll('\n', '').indexOf('S'), { x: pos % map[0].length, y: Math.floor(pos / map[0].length) });
 const end = (pos = input.replaceAll('\n', '').indexOf('E'), { x: pos % map[0].length, y: Math.floor(pos / map[0].length) });
-
 let adjs = [[-1, 0], [0, -1], [0, 1], [1, 0]];
+const getPossibleNeighbors = (from) => adjs.map(adj => (p = { x: from.x + adj[1], y: from.y + adj[0] }, p.key = p.x + ',' + p.y, p))
+    .filter(neighbor => map[neighbor.y] && map[neighbor.y][neighbor.x] >= 0 && map[neighbor.y][neighbor.x] - map[from.y][from.x] >= -1);
 
-const getNeighbors = (from) => adjs.map(adj => ({ x: from.x + adj[0], y: from.y + adj[1] }))
-    .filter(neighbor => map[neighbor.y] && map[neighbor.y][neighbor.x] >= 0 
-        && map[neighbor.y][neighbor.x] - map[from.y][from.x] >= -1);
-
-let mapDebug = map.map(l => l.map(e => 0));
 const visited = new Set();
-let  positions = [end], nextPositions = [] , dist = 1;
-while (positions) {
-    for (let current of positions) {
-        mapDebug[current.y][current.x] = 1;
-        let neighbors = getNeighbors(current);
-        for (let neighbor of neighbors) {
-            //if (neighbor.x === start.x && neighbor.y === start.y) throw "found it " + dist; // part 1
-            if (map[neighbor.y][neighbor.x] === 0) throw "found it " + dist; // part 2
-            if (!visited.has(neighbor.x + ',' + neighbor.y)) {
-                nextPositions.push(neighbor);
-                visited.add(neighbor.x + ',' + neighbor.y);
-            }
+let locations = [end], nextLocations = [], dist = 1;
+while (locations) {
+    for (let location of locations)
+        for (let neighbor of getPossibleNeighbors(location)) {
+            if (map[neighbor.y][neighbor.x] === 0) throw "found it " + dist; 
+            if (visited.has(neighbor.key)) continue;
+            nextLocations.push(neighbor);
+            visited.add(neighbor.key);
         }
-    }
-    positions = nextPositions, nextPositions = [];
+    locations = nextLocations, nextLocations = [];
     dist++;
 }
