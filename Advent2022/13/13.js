e4ed23df07fded29700b1cdbc13469113ec9f272
ZM <zerbini.morgan@gmail.com>
2022-12-13 22:09:25 +0100

    update day 13

--- Advent2022/13/13.js
+++ Advent2022/13/13.js
@@ -1,5 +1,4 @@
 const input = require('fs').readFileSync('./13/input.txt', 'utf-8');
-
 const signals = input.split("\n\n").map(s => s.split("\n").map(p => JSON.parse(p)));
 
 const compareSignal = (s1, s2) => {
@@ -15,9 +14,7 @@ const compareSignal = (s1, s2) => {
     if (Number.isInteger(s2)) return compareSignal(s1, [s2]);
     return 0;
 }
-
 const result = signals.map((s, i) => [i, 0 < compareSignal(s[0], s[1])])
     .filter(s => s[1]).map(s => s[0])
     .reduce((a, b) => a + b + 1, 0);
-
 console.log(result);
