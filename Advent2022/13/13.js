0a6e226165f3659da46e49274de04ebda800fb9b
ZM <zerbini.morgan@gmail.com>
2022-12-13 22:09:16 +0100

    update day 13

--- Advent2022/13/13.js
+++ Advent2022/13/13.js
@@ -4,15 +4,15 @@ const signals = input.split("\n\n").map(s => s.split("\n").map(p => JSON.parse(p
 
 const compareSignal = (s1, s2) => {
     if (Number.isInteger(s1) && Number.isInteger(s2)) return s2 - s1;
-    if (Number.isInteger(s1) && Array.isArray(s2)) return compareSignal([s1], s2);
-    if (Array.isArray(s1) && Number.isInteger(s2)) return compareSignal(s1, [s2]);
     if (Array.isArray(s1) && Array.isArray(s2))
-        while (s1.length > 0 || s2.length > 0) {
-            let a = s1.shift() ?? -1;
-            let b = s2.shift() ?? -1;
+        for (let i=0; i<=s1.length  || i<=s2.length ; i++) {
+            let a = s1[i] ?? -1;
+            let b = s2[i] ?? -1;
             const result = compareSignal(a, b);
             if (result != 0) return result;
         }
+    if (Number.isInteger(s1)) return compareSignal([s1], s2);
+    if (Number.isInteger(s2)) return compareSignal(s1, [s2]);
     return 0;
 }
 
