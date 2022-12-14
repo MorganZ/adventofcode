d36b0b968c02f3e5ec629124f5bfae3c8d367f0c
zm <morgan@z-rational.com>
2022-12-14 18:02:46 +0100

    template string

--- Advent2022/14/14.js
+++ Advent2022/14/14.js
@@ -4,20 +4,20 @@ xmax = Math.max(...rockWalls.flat(1).map(([x, y]) => y)) + 2;
 rockWalls.forEach(([[sx, sy], [ex, ey]]) => {
     [dx, dy] = [Math.sign(ex - sx), Math.sign(ey - sy)];
     while (true) {
-        theMap[sx + ',' + sy] = '#';
+        theMap[`${sx},${sy}`] = '#';
         if (sx === ex && sy === ey) break;
         sx += dx, sy += dy;
     }
 });
 
 for (step = 0; [y, x] = [500, 0] ;  step++) {
-    if (theMap[y + ',' + x]) break;
+    if (theMap[`${y},${x}`]) break;
     while (true) {
-        if (x + 1 == xmax) { theMap[y + ',' + x] = 'O'; break; }
-        if (!theMap[y + ',' + (x + 1)]) [y, x] = [y, x + 1]
-        else if (!theMap[(y - 1) + ',' + (x + 1)]) [y, x] = [y - 1, x + 1]
-        else if (!theMap[(y + 1) + ',' + (x + 1)]) [y, x] = [y + 1, x + 1]
-        else { theMap[y + ',' + x] = 'O'; break; }
+        if (x + 1 == xmax) { theMap[`${y},${x}`] = 'O'; break; }
+        if (!theMap[`${y},${x + 1}`]) [y, x] = [y, x + 1]
+        else if (!theMap[`${y - 1},${x + 1}`]) [y, x] = [y - 1, x + 1]
+        else if (!theMap[`${y + 1},${x + 1}`]) [y, x] = [y + 1, x + 1]
+        else { theMap[`${y},${x}`] = 'O'; break; }
     }
 }
 console.log(step);
