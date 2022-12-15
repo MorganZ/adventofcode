6834390ef346468adb6e809af5bae49e2a1553ec
ZM <zerbini.morgan@gmail.com>
2022-12-15 20:42:27 +0100

    rework day 15 part 2

--- Advent2022/15/15_2.js
+++ Advent2022/15/15_2.js
@@ -1,46 +1,23 @@
-//Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-//parse this line to get the coordinates of the sensor and beacons
-
 const sensorData = require('fs').readFileSync('./15/input.txt', 'utf-8').split('\n')
     .map(line => {
-        const regex = /x=(-?\d+), y=(-?\d+)/g;
-        const xyCoordinates = line.match(regex).map(coordinate => coordinate.split(","));
-        const xyCoordinatesInt = xyCoordinates.map(coordinate => coordinate.map(c => parseInt(c.split("=")[1])));
-        return xyCoordinatesInt;
-    })
+        const xyMatches = line.match(/x=(-?\d+), y=(-?\d+)/g).map(coordinate => coordinate.split(","));
+        const coordinates = xyMatches.map(coordinate => coordinate.map(c => parseInt(c.split("=")[1])));
+        return coordinates;
+    }).sort((a, b) => a[0][1] - b[0][1]);
 
+let ymin = sensorData[0][0][1];
+let ymax = sensorData[sensorData.length - 1][0][1];
 
-
-for (let line = 0; line < 4000000; line++) {
+for (let y = ymin; y < ymax; y++) {
     const possiblePoints = [];
     sensorData.forEach(([sPos, bPos]) => {
         const distance = Math.abs(sPos[0] - bPos[0]) + Math.abs(sPos[1] - bPos[1]);
-        const distanceToLine = Math.abs(sPos[1] - line);
-
-        let left = distance - distanceToLine
-        if( 0< left) {
-            possiblePoints.push([sPos[0]-left, sPos[0]+left]);
-        }
+        const distanceToLine = Math.abs(sPos[1] - y);
+        let left = distance - distanceToLine;
+        if (0 < left) possiblePoints.push([sPos[0] - left, sPos[0] + left]);
     });
-
-    var range = possiblePoints.sort((a, b) => a[0] - b[0]);
-    range.reduce((p, c) => {
-        if (p[1] >= c[0]) {
-            if(p[1] < c[1]) return c;
-            else return p;
-        }
-        else {
-            var [x, y ] = [p[1]  + 1, line]
-            console.log(x,y);
-            console.log(x*4000000+y);
-            throw new Error("Not possible");
-        }
+    possiblePoints.sort((a, b) => a[0] - b[0]).reduce((p, c) => {
+        if (p[1] >= c[0]) return p[1] < c[1] ?  c : p;
+        else throw new Error(p[1] * 4000000 + y);
     });
 }
-
-
-possibleBeacons.forEach(beacon => {
-    possiblePoints.delete(beacon);
-});
-
-console.log(possiblePoints.size);
